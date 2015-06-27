13c35dc1b386935a207679619988aa5f61c1cea7
Daniel Norberg <dano@spotify.com>
2015-06-27 11:46:50 -0400

    use vert.x

--- src/main/java/io/norberg/netty_openssl_test/NettyOpenSslTest.java
+++ src/main/java/io/norberg/netty_openssl_test/NettyOpenSslTest.java
@@ -1,27 +1,42 @@
 package io.norberg.netty_openssl_test;
 
-import com.mastfrog.netty.http.client.HttpClient;
-import com.mastfrog.netty.http.client.ResponseFuture;
-import com.mastfrog.netty.http.client.ResponseHandler;
-
 import java.io.IOException;
+import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.ExecutionException;
 
-import io.netty.handler.codec.http.HttpHeaders;
-import io.netty.handler.codec.http.HttpResponseStatus;
+import io.vertx.core.Vertx;
+import io.vertx.core.http.HttpClient;
+import io.vertx.core.http.HttpClientOptions;
+import io.vertx.core.http.HttpClientRequest;
 
 public class NettyOpenSslTest {
 
   public static void main(final String... args)
       throws ExecutionException, InterruptedException, IOException {
 
-    final HttpClient client = HttpClient.builder().followRedirects().build();
+    final Vertx vertx = Vertx.vertx();
 
-    final ResponseFuture h = client.get().setURL("https://www.google.com/").execute(
-        new ResponseHandler<String>(String.class) {
-          protected void receive(HttpResponseStatus status, HttpHeaders headers, String response) {
-            System.out.println(response);
-          }
-        });
+    final HttpClient client = vertx.createHttpClient(
+        new HttpClientOptions().setSsl(true));
+
+    final CompletableFuture<Void> done = new CompletableFuture<>();
+
+
+    final HttpClientRequest request = client.getAbs("https://www.google.com:443/");
+    request.handler(event -> {
+      System.out.println(event.statusCode() + " " + event.statusMessage());
+      event.bodyHandler(buf -> {
+        final String body = buf.toString("UTF-8");
+        System.out.println(body);
+        done.complete(null);
+      });
+    });
+
+    request.end();
+
+    done.get();
+
+    client.close();
+    vertx.close();
   }
 }
